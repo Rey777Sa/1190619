fe555b1ba4afbd86fd2c7546758167375c3a3419
Rey777Sa <51958301+Rey777Sa@users.noreply.github.com>
2019-06-30 21:46:44 -0500

    PRIMEERA VERSION

---
+++ src/main/java/com/example/satellite/biblioteca/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.satellite.biblioteca;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText txt2;
+    private EditText txt3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
